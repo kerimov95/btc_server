374a9366089e261b54b8fe3fe98d4b11552040b0
Kerimov <kerimov@mes-MacBook-Pro.local>
2022-07-16 15:14:18 +0300

    fixed changes

--- rpc/scripts/btc_start.sh
+++ rpc/scripts/btc_start.sh
@@ -6,7 +6,7 @@ rm -R btc
 mkdir -p btc/node1 btc/node2
 
 echo -e "rpcuser=username\nrpcpassword=password" > btc/node1/bitcoin.conf
-echo -e "rpcuser=username\nrpcpassword=password\nwalletnotify=curl http://127.0.0.1:4444/api/wallet/wehook/%s" > btc/node2/bitcoin.conf
+echo -e "rpcuser=username\nrpcpassword=password\nwalletnotify=curl http://127.0.0.1:4444/api/merchant/hook/BTC/%s" > btc/node2/bitcoin.conf
 
 bitcoind -regtest -port=1111 -datadir=btc/node1 -rpcport=1234 -fallbackfee=0.00001 --daemon
 bitcoind -regtest -port=2222 -datadir=btc/node2 -rpcport=9555 -fallbackfee=0.00001 --daemon
